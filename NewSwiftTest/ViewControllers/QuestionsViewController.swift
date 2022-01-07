4dff0517798ddafc4f49a41c56031a1e7848c994
Arslan Abdullaev <ars352@yahoo.com>
2022-01-07 20:37:46 +0300

    refactoring questionsvc

---
+++ NewSwiftTest/ViewControllers/QuestionsViewController.swift
@@ -0,0 +1,69 @@
+//
+//  QuestionsViewController.swift
+//  NewSwiftTest
+//
+//  Created by Кирилл Тараско on 07.01.2022.
+//
+
+import UIKit
+
+class QuestionsViewController: UIViewController {
+  
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBOutlet weak var question: UILabel!
+    
+    @IBOutlet var buttonAnswers: [UIButton]!
+    
+    let questionManager = DataManager()
+    var totalProgress = DataManager().totalAnswers
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        //newQuestion()
+        progressBar.setProgress(getProgress(), animated: true)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let resultVC = segue.destination as! ResultsViewController
+        resultVC.totalAnswers = questionManager.totalAnswers
+        resultVC.totalRightAnswers = questionManager.totalRightAnswers
+    }
+    
+    @IBAction func selectAnswer(_ sender: UIButton) {
+        let index = buttonAnswers.firstIndex(of: sender)!
+        questionManager.validateAnswer(index: index)
+    }
+   
+}
+
+extension QuestionsViewController {
+    func newQuestion() {
+        questionManager.refreshTest()
+        question.text = questionManager.question
+        
+        for index in 0..<questionManager.possibleAnswers.count {
+            let possibleAnswer = questionManager.possibleAnswers[index]
+            let button = buttonAnswers[index]
+            
+            button.setTitle(possibleAnswer, for: .normal)
+        }
+    }
+    
+    func showResult() {
+        performSegue(withIdentifier: "result", sender: nil)
+    }
+    
+    func getProgress() -> Float {
+        let progress = Float(totalProgress / questionManager.question.count)
+        return progress
+    }
+
+    
+}
+    
+
+
---
+++ NewSwiftTest/ViewControllers/ResultsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ResultsViewController.swift
+//  NewSwiftTest
+//
+//  Created by Кирилл Тараско on 07.01.2022.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    var totalRightAnswers: Int = 0
+    var totalAnswers: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
