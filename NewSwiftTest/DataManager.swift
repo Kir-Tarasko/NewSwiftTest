f00067f8c8a465eaf1d347a50aab1941f69ef853
Кирилл Тараско <tarasko.kirill@icloud.com>
2022-01-07 17:05:28 +0200

    Work in progress

--- NewSwiftTest/DataManager.swift
+++ NewSwiftTest/DataManager.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+
+
+class DataManager {
+    private let questions :[(question: String, rightAnswer: String, possibleAnswers: [String])] = [(question: "", rightAnswer: "", possibleAnswers: ["", "", "", ""])]
+    
+    private var test: Test!
+    private var _totalAnswers = 0
+    private var _totalRightAnswers = 0
+    
+    var question: String {
+        return test.question
+    }
+    
+    var possibleAnswers: [String] {
+        return test.possibleAnswers
+    }
+    
+    var totalAnswers: Int {
+        return _totalAnswers
+    }
+    
+    var totalRightAnswers: Int {
+        return _totalRightAnswers
+    }
+    
+    func refreshTest() {
+        let randomIndex = Int.random(in: 0..<25)
+        let testData = questions[randomIndex]
+        
+        test = Test(question: testData.question, possibleAnswers: testData.possibleAnswers, rightedAnswer: testData.rightAnswer)
+    }
+    
+    func validateAnswer(index: Int) {
+        _totalAnswers += 1
+        if test.validatePossibleAnswer(index){
+            _totalRightAnswers += 1
+        }
+    }
+
+}
